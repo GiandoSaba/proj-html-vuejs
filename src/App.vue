6d45a5e9cfdcdd72e7b466967632141577f17f2b
GiandoSaba <giandosaba997@gmail.com>
2022-01-24 10:42:58 +0100

    add comments

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,35 @@
 <template>
   <div id="app">
-
+  <!-- Header -->
+    <!-- Header-Nav -->
+    <!-- /Header-Nav -->
+    <!-- Jumbotron -->
+    <!-- /Jumbotron -->
+  <!-- /Header -->
+  <!-- Main -->
+    <!-- Section-Numbers -->
+    <!-- /Section-Numbers -->
+    <!-- Section-AboutMe -->
+    <!-- /Section-AboutMe -->
+    <!-- Section-LatestCourse -->
+    <!-- /Section-LatestCourse -->
+    <!-- Section-BookStore -->
+    <!-- /Section-BookStore -->
+    <!-- Section-Ico -->
+    <!-- /Section-Ico -->
+    <!-- Section-Testimonial -->
+    <!-- /Section-Testimonial -->
+    <!-- Section-Articles -->
+    <!-- /Section-Articles -->
+  <!-- /Main -->
+  <!-- Footer -->
+    <!-- Info -->
+    <!-- /Info -->
+    <!-- Link -->
+    <!-- /Link -->
+    <!-- Copyright -->
+    <!-- /Copyright -->
+  <!-- /Footer -->
   </div>
 </template>
 
