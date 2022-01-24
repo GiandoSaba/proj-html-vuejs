47b31a7ee55ce495ebc5f68efe9be77e96cc197d
GiandoSaba <giandosaba997@gmail.com>
2022-01-24 18:26:58 +0100

    about section finita

--- src/components/Main_subcomponent/SectionAbout.vue
+++ src/components/Main_subcomponent/SectionAbout.vue
@@ -1,18 +1,18 @@
 <template>
   <section class="py-5">
     <div class="row">
-      <div class="col-12 mb-2">
+      <div class="col-12">
         <h2 class="subtitle text-green fs-5">
           About Me
         </h2>
       </div>
-      <div class="col-4">
+      <div class="col-4 mb-5 mt-3">
         <h1 class="title fs-1 pd-5 md-4 lh-base">
           How to Be an Alpha Male:
           The 15 Habits of True Alphas
         </h1>
       </div>
-      <div class="offset-2 col-6">
+      <div class="offset-2 col-6 mb-5 mt-3">
         <h2 class="fs-6 fw-bold lh-lg">
           Want to learn how to be <span class="text-green">an alpha male</span>?
           How to be more masculine?
@@ -24,6 +24,36 @@
           explain how these qualities are applicable to your work.
           And the important thing is you do love what you do.
         </p>
+      </div>
+      <div class="col-6 my-5 image-container">
+        <img
+          src="../../assets/img/home-movation-video-poster-670x450.jpg"
+          alt="home-movation-video-poster"
+          class="image-rounded img-fluid"
+        >
+        <img
+          src="../../assets/img/icon-youtube-play.png"
+          alt="icon-youtube-play"
+          class="youtube-logo"
+        >
+      </div>
+      <div class="col-4 m-auto pe-5">
+        <h2 class="title fs-4 lh-lg">
+          "Every morning you have two choices: continue
+          to sleep with your dreams, or wake up and
+          chase them."
+        </h2>
+        <img
+          src="../../assets/img/home-movation-shape-01.png"
+          alt="home-movation-shape"
+          class="img-fluid mt-5 mb-2"
+        >
+        <h3 class="fw-bold fs-5 my-2 mt-3">
+          Kaixa Stark
+        </h3>
+        <h4 class="subtitle text-green fs-6">
+          WingMan
+        </h4>
       </div>
     </div>
   </section>
@@ -37,5 +67,20 @@ export default {
 
 <style scoped lang="scss">
 @import '../../assets/scss/style';
+.image-container {
+  position: relative;
+  overflow: hidden;
+  border-radius: 10px;
+  .image-rounded:hover{
+    transition: 1s linear;
+    transform: scale(1.1);
+  }
+  .youtube-logo {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,-50%);
+  }
+}
 
 </style>
